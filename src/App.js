0735dfdca56794663284c5e6e7c2242f98c8c17f
Dmitriy Bondarenko <demianbondarenko@gmail.com>
2020-10-05 12:37:05 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.scss';
+import ProductList from "./components/ProductList";
+
+function App() {
+  return (
+    <div className="App">
+      <main className="main">
+        <div className="container">
+          <h1>Ты сегодня покормил кота?</h1>
+          <ProductList />
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
