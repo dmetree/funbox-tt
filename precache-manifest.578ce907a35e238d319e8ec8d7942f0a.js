3579a345138afe081ccfca181f0cc0c47ce03182
Dmitriy Bondarenko <demianbondarenko@gmail.com>
2020-10-05 12:38:28 +0200

    Updates

---
+++ precache-manifest.578ce907a35e238d319e8ec8d7942f0a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64e4f97a2000a5824493829f5e98e764",
+    "url": "/funbox-tt/index.html"
+  },
+  {
+    "revision": "fa57e34f5aa82d261b95",
+    "url": "/funbox-tt/static/css/main.9193f4ab.chunk.css"
+  },
+  {
+    "revision": "5fc9330d705a572993ee",
+    "url": "/funbox-tt/static/js/2.71a6bf5e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/funbox-tt/static/js/2.71a6bf5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa57e34f5aa82d261b95",
+    "url": "/funbox-tt/static/js/main.d1391227.chunk.js"
+  },
+  {
+    "revision": "3fb963cc68d3895db56f",
+    "url": "/funbox-tt/static/js/runtime-main.8faf75bd.js"
+  },
+  {
+    "revision": "7b435128f4aa5d3f6202e35a339643c0",
+    "url": "/funbox-tt/static/media/exo20-thin.7b435128.woff2"
+  },
+  {
+    "revision": "b7f2073db20fbe66e67c7b28189c9f3b",
+    "url": "/funbox-tt/static/media/exo20-thin.b7f2073d.woff"
+  }
+]);
